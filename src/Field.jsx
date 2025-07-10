0fbe2eb7d0608067296e15ec796413cd9fbdd4eb
Mark Lawrence <mark.lawrence@fullstackacademy.com>
2025-07-09 21:14:48 -0400

    update Field.jsx

--- src/Field.jsx
+++ src/Field.jsx
@@ -1,12 +1,15 @@
 export default function Field() {
   const NUM_HOLES = 9;
+  const newField = Array(NUM_HOLES).fill(false);
+
   // Pick a random index for the mole
-  const moleIndex = Math.floor(Math.random() * NUM_HOLES);
+  let mole = Math.floor(Math.random() * NUM_HOLES);
+  newField[mole] = true;
 
   return (
     <ul className="field">
-      {Array.from({ length: NUM_HOLES }).map((_, i) => (
-        <li key={i} className={`hole${i === moleIndex ? " mole" : ""}`}></li>
+      {newField.map((hasMole, i) => (
+        <li key={i} className={`hole${hasMole ? " mole" : ""}`}></li>
       ))}
     </ul>
   );
